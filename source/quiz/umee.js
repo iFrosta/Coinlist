7abfda2a2b49240dc9e5a49a0369f8f796478063
Sam <samgolovchenko@gmail.com>
2021-11-20 01:10:12 +0300

    umee

---
+++ source/quiz/umee.js
@@ -0,0 +1,28 @@
+let run = async () => {
+  // https://github.com/iFrosta/Coinlist
+  let input = document.body.querySelectorAll(".quiz")[0].querySelectorAll('.c-label--inline'),
+    timeout = (ms) => new Promise(resolve => setTimeout(resolve, ms)),
+    dict = [
+      "Option 1: 300,000,000; Option 2: 200,000,000",
+      "Users in the waiting room for the sale will be given a random spot in the queue when the sale starts. Users who arrive after the sale starts for the sale will be placed behind those in the waiting room",
+      "Multichain",
+      "Cross chain lending hub",
+      "Option 1: 50% of the token purchase amount is freely trading 40 days after the sale date on or around December 21, 2021, and the other 50% of the token purchase amount unlocks 12 months after the sale date on or around November 10, 2022. Option 2: Tokens unlock 12 months after the sale date on or around November 10, 2022",
+      "BTC, ETH, USDC, USDT",
+      "Option 1: $0.06 per token, $500 limit. Option 2: $0.07 per token, $500 limit",
+      "The user's purchase may be cancelled and the user may be banned from future CoinList sales ",
+      "CoinList.co",
+      "The user's account will be terminated and all purchases will be cancelled"
+    ]
+  
+  console.log('❤️ Thank you for your support ERC20 | BEP20 (BSC)\n0x8e6a359C05a8ab21409F74ec175Fb0C5ee4CF38c')
+  
+  for (let i = 0; i < input.length; i++) {
+    if (dict.indexOf(input[i].textContent) !== -1) input[i].querySelectorAll('.c-input')[0].checked = true
+  }
+  
+  await timeout(200)
+  document.body.querySelector('.js-submit').click()
+}
+
+run()
